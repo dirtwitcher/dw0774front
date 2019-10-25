f1a3cf2b202e127a3b1e5598e1970f143d296d7a
Vladislav <vladikbirkos@yandex.ru>
2019-10-25 18:42:19 +0300

    25.10

--- src/app/auto/auto.component.ts
+++ src/app/auto/auto.component.ts
@@ -1,20 +1,14 @@
-import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
-class Auto {
+
+export class Auto {
   win: string;
   toplivo: string;
   privod: string;
   probeg: number;
   cvet: string;
   dopComment: string;
-}
-
-class DataTablesResponse {
-  data: any[];
-  draw: number;
-  recordsFiltered: number;
-  recordsTotal: number;
 }
 
 @Component({
@@ -25,6 +19,8 @@ class DataTablesResponse {
 
 export class AutoComponent implements OnInit {
 
+  private autos: Auto[] = [];
+
   win: string;
   toplivo: string;
   privod: string;
@@ -32,13 +28,11 @@ export class AutoComponent implements OnInit {
   cvet: string;
   dopComment: string;
 
-  dtOptions: DataTables.Settings = {} ;
-  autos: Auto[];
+  dtOptions: DataTables.Settings = {};
 
   constructor(private http: HttpClient) {}
 
   addAuto(){
-
     var myData = {"win": this.win,
     "toplivo": this.toplivo,
     "privod": this.privod,
@@ -46,57 +40,24 @@ export class AutoComponent implements OnInit {
     "cvet": this.cvet,
     "dopComment": this.dopComment
     };        
+
     jQuery.ajax({
         url: "http://127.0.0.1:8080/diplomBackEnd/Auto",
         data: JSON.stringify(myData),
-        success: function(){
-            console.log(JSON.stringify(myData));
-        },
+        success: function(data){
+          alert(data)
+      }, 
         error: function(data) {
             console.log("Error: ", data);
         },
         type: "post",
+        dataType: "text",
         timeout: 30000
     });
-
-/*
-    console.log( this.win + ' ' + this.toplivo + ' ' + this.privod + ' ' + this.probeg + ' ' + this.cvet + ' ' + this.dopComment);
-    this.http.post('http://127.0.0.1:8080/diplomBackEnd/Auto', {
-      'win': this.win,
-      'toplivo': this.toplivo,
-      'privod': this.privod,
-      'probeg': this.probeg,
-      'cvet': this.cvet,
-      'dopComment': this.dopComment
-    });
-    */
+    // console.log("list: ", this.autos);
   }
 
   ngOnInit(): void {
-  /*  const that = this;
-
-    this.dtOptions = {
-      pagingType: 'full_numbers',
-      pageLength: 2,
-      serverSide: true,
-      processing: true,
-      ajax: (dataTablesParameters: any, callback) => {
-        that.http
-          .post<DataTablesResponse>(
-            'http://localhost:8080/diplomBackEnd/Auto',
-            dataTablesParameters, {}
-          ).subscribe(resp => {
-            that.autos = resp.data;
-
-            callback({
-              recordsTotal: resp.recordsTotal,
-              recordsFiltered: resp.recordsFiltered,
-              data: []
-            });
-          });
-      },
-      columns: [{ data: 'win' }, { data: 'toplivo' }, { data: 'privod' }, 
-                { data: 'probeg' }, { data: 'cvet' }, { data: 'dopComment' }]
-    };*/
   }
+
 }
